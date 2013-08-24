563b68a6cd0dd6cde8444aa510d42195a15b48de
Garrett Robinson <garrettr@riseup.net>
2013-08-23 21:45:31 -0700

    start of imap polling bot

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 OpenPGPBot
 ==========
+
+## Getting Started
+
+This is a Python project with some external dependencies. We recommend using
+virtualenv:
+
+    $ virtualenv env
+    $ . env/bin/activate
+    (env) $ pip install -r requirements.txt
+    (env) $ ./bot.py
+    # when you're done
+    (env) $ deactivate
+
+At the moment, `bot.py` expects to find `config.py` with information about the
+email server it is to talk to. Since `config.py` contains sensitive
+authentication credentials, it is not included in this repo (it's in .gitignore
+as well to prevent accidents). The easiest way to get going is to `cp
+config_template.py` to `config.py` and fill in the appropriate values.
+
+## Dev Notes
+
+1. [OpenPGP RFC 4880](http://tools.ietf.org/html/rfc4880)
+2. [MIME RFC 2045](http://tools.ietf.org/html/rfc2045)
+3. [Helpful tutorial for Python's imaplib](http://yuji.wordpress.com/2011/06/22/python-imaplib-imap-example-with-gmail/)
---
+++ bot.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+"""
+An email bot to help you learn OpenPGP!
+"""
+
+import sys
+import imaplib
+import email
+
+import gnupg
+
+PGP_ARMOR_HEADER_MESSAGE   = "-----BEGIN PGP MESSAGE-----"
+PGP_ARMOR_HEADER_SIGNATURE = "-----BEGIN PGP SIGNATURE-----"
+
+try:
+    import config
+except ImportError:
+    print >> sys.stderr, "Error: could not load configuration from config.py"
+    sys.exit(1)
+
+def login(username, password, imap_server="imap.gmail.com"):
+    mail = imaplib.IMAP4_SSL(imap_server)
+    mail.login(username, password)
+    return mail
+
+def get_all_mail():
+    mail = login(config.USERNAME, config.PASSWORD)
+    mail.select("inbox")
+    # Get all email in the inbox (with uids instead of sequential ids)
+    result, data = mail.uid('search', None, "ALL")
+    # result should be 'OK'
+    # data is a list with a space separated list of ids
+    message_ids = data[0].split()
+    messages = []
+    for message_id in message_ids:
+        # fetch the email body (RFC822) for the given ID
+        result, data = mail.uid('fetch', message_id, "(RFC822)")
+        # convert raw email body into EmailMessage
+        messages.append(email.message_from_string(data[0][1]))
+    return mail, message_ids, messages
+
+def is_pgp_email(email):
+    # XXX: rough heuristic. This is probably quite nuanced among different
+    # clients.
+    # 1. Multipart and non-multipart emails
+    # 2. ASCII armored and non-armored emails
+    encrypted, signed = False, False
+    for part in email.walk():
+        if part.get_content_type() in ("text/plain", "text/html",
+                "application/pgp-signature"):
+            payload = part.get_payload()
+            if PGP_ARMOR_HEADER_MESSAGE in payload:
+                encrypted = True
+            elif PGP_ARMOR_HEADER_SIGNATURE in payload:
+                signed = True
+            else:
+                # TODO: might not be ASCII armored. Trial
+                # decryption/verification?
+                pass
+    return encrypted, signed
+
+def main():
+    gpg = gnupg.GPG(homedir="bot_keyring")
+    imap_conn, message_ids, messages = get_all_mail()
+    for message in messages:
+        encrypted, signed = is_pgp_email(message)
+        if encrypted:
+            print '"%s" from %s is encrypted' % (message['Subject'], message['From'])
+        if signed:
+            print '"%s" from %s is signed' % (message['Subject'], message['From'])
+
+if __name__ == "__main__":
+    main()
